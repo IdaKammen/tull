e2aecaaf999a5a0b34b0789e16e7d9fc736d0050
Ida Borset Kammen <54891608+IdaKammen@users.noreply.github.com>
2019-10-11 10:21:59 +0200

    Add files via upload

---
+++ presmodel.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <title>API</title>
+</head>
+
+<body>
+
+    <h1 id=""> Hello!</h1>
+    
+</body>
+
+<script type ="text/javascript">
+
+
+</script>
+
+</html>
